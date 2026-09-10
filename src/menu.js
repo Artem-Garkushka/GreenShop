17b48738567a48878f14eb98110327784e3cb040
Artem Garkushka <Artem.Garkushka@gmail.com>
2026-09-10 18:15:14 +0300

    зробив header

---
+++ src/menu.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // Підтягуємо header.html
+  fetch("/src/header/header.html")
+    .then((res) => res.text())
+    .then((html) => {
+      document.getElementById("header").innerHTML = html;
+
+      // Автоматичне підсвічування активного пункту
+      document.querySelectorAll(".header-list-nav a").forEach((link) => {
+        if (link.href.includes(window.location.pathname)) {
+          link.classList.add("active");
+        }
+      });
+    });
+});
