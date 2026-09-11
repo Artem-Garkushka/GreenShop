92358b36cff445d642d2d8c36437d65f2a17933d
Artem Garkushka <Artem.Garkushka@gmail.com>
2026-09-11 19:39:00 +0300

    header

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
   fetch("./src/header/header.html")
     .then(response => response.text())
   .then(data => {
-      document.getElementById("header").innerHTML = html;
+      document.getElementById("header").innerHTML = data;
 
       // Автоматичне підсвічування активного пункту
       document.querySelectorAll(".header-list-nav a").forEach((link) => {
